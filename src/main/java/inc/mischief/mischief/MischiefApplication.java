3402a5831dabd6c7063a632d13e93f081a5ec3c6
Nikolay Doronkin Mejia <nikolay.daronkin-mejia@innowise-group.com>
2023-02-02 23:08:50 +0300

    Initial commit

---
+++ src/main/java/inc/mischief/mischief/MischiefApplication.java
@@ -0,0 +1,13 @@
+package inc.mischief.mischief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MischiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MischiefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/mischief/mischief/MischiefApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.mischief.mischief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MischiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
